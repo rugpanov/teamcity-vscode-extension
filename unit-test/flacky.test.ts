96bdc4de87c3fbffa661cb65810808b359c0b9c1
Grigory Panov <gripanov@gmail.com>
2018-04-02 14:08:44 +0300

    Increase success rate

--- unit-test/flacky.test.ts
+++ unit-test/flacky.test.ts
@@ -2,6 +2,6 @@ import {assert} from "chai";
 
 suite("Flacky test", () => {
     test("Hi! I am a flacky test with probability 0.25.", function () {
-        assert.isTrue(Math.random() >= 0.75);
+        assert.isTrue(Math.random() >= 0.25);
     });
 });
