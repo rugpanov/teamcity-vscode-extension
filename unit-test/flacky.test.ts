dcbb2995e12ce5beeb9ee933a77a34471566f0cc
Grigory Panov <gripanov@gmail.com>
2018-04-17 15:29:20 +0300

    Increase fealure rate

--- unit-test/flacky.test.ts
+++ unit-test/flacky.test.ts
@@ -1,7 +1,7 @@
 import {assert} from "chai";
 
 suite("Flacky test", () => {
-    test("Hi! I am a flacky test with probability 0.25.", function () {
-        assert.isTrue(Math.random() >= 0.25);
+    test("Hi! I am a flacky test with probability 0.50.", function () {
+        assert.isTrue(Math.random() >= 0.50);
     });
 });
