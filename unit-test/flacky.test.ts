6df623bcfd473ed60f3e9400b79ffab6714b926f
Grigory Panov <gripanov@gmail.com>
2018-07-12 12:36:03 +0300

    Fix flaky test

--- unit-test/flacky.test.ts
+++ unit-test/flacky.test.ts
@@ -2,6 +2,6 @@ import {assert} from "chai";
 
 suite("Flacky test", () => {
     test("Hi! I am a flacky test with probability 0.50.", function () {
-        assert.isTrue(Math.random() >= 0.50);
+        assert.isTrue(Math.random() >= 0);
     });
 });
