3b3ac506c276e0d6b417541770f6af3581db80a5
Grigory Panov <gripanov@gmail.com>
2018-11-20 16:23:31 +0300

    up

--- unit-test/bll/cvsutils/gitpathfinder.test.ts
+++ unit-test/bll/cvsutils/gitpathfinder.test.ts
@@ -141,7 +141,7 @@ suite("Git Path Finder", () => {
         const gitPathFinder: GitPathFinder = new GitPathFinder(cpSpy, processMock, fsSpy, workspaceSpy);
         gitPathFinder.find().then((gitPath) => {
                 assert.equal(gitPath, expectedGitPath);
-                done();
+                done("broken");
             }
         ).catch((err) => {
             done(err);
